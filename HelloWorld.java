3435851a85a4535dd47521757491cf70fa973afc
AsAnt <93052826+AsiminaAnt@users.noreply.github.com>
2021-11-11 21:57:57 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	
 	private static int heartwise=0;
 	public static void main(String[] args) {
-        System.out.println("Hello , World!");
+        System.out.println("Hello whole, World!");
        	int a = 5;
         a = 0 - a;
         System.out.print(a);
