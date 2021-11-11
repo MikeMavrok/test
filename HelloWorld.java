51c200962166934b7c3ae48e99c745b11bec6267
MikeMavrok <93074951+MikeMavrok@users.noreply.github.com>
2021-11-11 19:08:18 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	private static int heartwise=0;
 	public static void main(String[] args) {
         System.out.println("Hello, World!");
-       int a = 5;
+       	int a = 5;
         a = 0 - a;
         System.out.print(a);
         System.out.println(heartwise);
